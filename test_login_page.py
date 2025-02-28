c9f1c1fd62ac1117671cc20b76f61ff4949e607e
Elena B <trudoluba@mail.ru>
2025-03-01 02:20:48 +0300

    transition b/w pages

--- test_login_page.py
+++ test_login_page.py
@@ -2,7 +2,6 @@ from .pages.login_page import LoginPage
 import pytest
 from selenium.webdriver.common.by import By
 
-#link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
 def test_guest_should_see_login_url(browser):
