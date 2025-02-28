73fd1eda4b30094ed672b3370a2a12200b94605a
Elena B <trudoluba@mail.ru>
2025-03-01 02:03:41 +0300

    LoginPage

---
+++ test_login_page.py
@@ -0,0 +1,22 @@
+from .pages.login_page import LoginPage
+import pytest
+from selenium.webdriver.common.by import By
+
+#link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+
+def test_guest_should_see_login_url(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+    
+def test_guest_should_see_register_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()    
