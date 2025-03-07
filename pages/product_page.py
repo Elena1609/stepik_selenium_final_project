a9ba15a56b9ff6dccb6ffccb1d736601f436942f
Elena B <trudoluba@mail.ru>
2025-03-08 02:12:55 +0300

    final assignment

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,7 +8,7 @@ class ProductPage(BasePage):
         book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME_LINK).text
         cart_link = self.browser.find_element(*ProductPageLocators.CART_LINK)
         cart_link.click()
-        self.solve_quiz_and_get_code()
+        #self.solve_quiz_and_get_code()
         cart_price = self.browser.find_element(*ProductPageLocators.CART_PRICE_LINK).text
         assert price in cart_price, "Стоимость корзины не совпадает с ценой товара!"
         message = self.browser.find_element(*ProductPageLocators.MESSAGE_LINK).text
